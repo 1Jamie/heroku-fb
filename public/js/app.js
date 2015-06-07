41ab48f043f96e35d57200208520b53598d3e6d4
charja13@live.com <charja13@live.com>
2015-06-06 22:19:40 -0400

    correcting syntax

--- public/js/app.js
+++ public/js/app.js
@@ -29,7 +29,6 @@ function statusChangeCallback(response) {
 //retrieving feed
 var findFeed = function() {
     FB.api('me?fields=id,name,posts.limit(25)',function(response){
-      console.log('fetching feed for: ' response.name)
       console.log(response);
       var newTextContent = [];
       for (var n=0; n<response.posts.data.length; n++) {
