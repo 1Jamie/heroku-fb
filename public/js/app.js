e803fdfb7e5602eac89c50b73f1c06d735c3af9f
1Jamie <charja13@live.com>
2015-07-27 23:44:20 -0400

    another increase to retreive wait time

--- public/js/app.js
+++ public/js/app.js
@@ -279,7 +279,7 @@ var postStuff = function(stuff) {
             if (!response && !error.response) {
                 console.log('an error occured');
             } else {
-                setInterval(findFeed(), 3500);
+                setInterval(findFeed(), 4000);
                 document.getElementById('story').value = '';
                 console.log('connected and post was made');
                 console.log(response);
